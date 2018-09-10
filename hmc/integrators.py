c437019a887ac3ffca9871d4b3aa8d3428f9736b
Matt Graham <matthew.m.graham@gmail.com>
2018-09-10 20:36:08 +0800

    Reorganising and fixing constrained integrator classes.

--- hmc/integrators.py
+++ hmc/integrators.py
@@ -148,55 +148,75 @@ class GeneralisedLeapfrogIntegrator(object):
         return state
 
 
-class GeodesicLeapfrogIntegrator(object):
+class BaseConstrainedLeapfrogIntegrator(object):
     """Leapfrog iterator for constrained separable Hamiltonian systems."""
 
-    def __init__(self, system, step_size, n_inner_step=1, tol=1e-8,
-                 max_iters=100):
+    def __init__(self, system, step_size, n_inner_step=1,
+                 reverse_check_tol=1e-8, reverse_check_norm=maximum_norm):
         self.system = system
         self.step_size = step_size
         self.n_inner_step = n_inner_step
-        self.tol = tol
-        self.max_iters = max_iters
-
-    def project_mom(self, mom, dc_dpos, chol_gram):
-        mom -= dc_dpos.T @ sla.cho_solve(chol_gram, dc_dpos @ mom)
-
-    def project_pos(self, pos, dc_dpos, chol_gram):
-        for i in range(self.max_iters):
-            delta = self.system.constr_func(pos)
-            if max_abs(delta) < self.tol:
-                return pos
-            pos -= dc_dpos.T @ sla.cho_solve(chol_gram, delta)
-        err = max_abs(self.system.constr_func(pos))
-        raise IntegratorError(
-            f'Quasi-Newton iteration did not converge. Last error {err:.1e}.')
+        self.reverse_check_tol = reverse_check_tol
+        self.reverse_check_norm = reverse_check_norm
 
     def step_a(self, state, dt):
-        state.mom -= dt * self.system.dh_dpos(state)
-        self.project_mom(state.mom, state.dc_dpos, state.chol_gram)
+        raise NotImplementedError()
+
+    def step_b_inner(self, state, dt):
+        raise NotImplementedError()
+
+    def solve_for_mom_post_projection(self, state, state_prev, dt):
+        raise NotImplementedError()
 
     def step_b(self, state, dt):
         dt_i = dt / self.n_inner_step
         for i in range(self.n_inner_step):
-            pos_i = state.pos
-            state.pos = state.pos + dt_i * state.mom
-            self.project_pos(state.pos, state.dc_dpos, state.chol_gram)
-            self.system.update_constr_jacob_and_chol_gram(state)
-            state.mom = (state.pos - pos_i) / dt_i
-            self.project_mom(state.mom, state.dc_dpos, state.chol_gram)
-            pos_r = state.pos - dt_i * state.mom
-            self.project_pos(pos_r, state.dc_dpos, state.chol_gram)
-            rev_diff = max_abs(pos_i - pos_r)
-            if rev_diff > 2 * self.tol:
+            state_prev = state.copy()
+            self.step_b_inner(state, dt_i)
+            self.system.project_onto_manifold(state, state_prev)
+            self.solve_for_mom_post_projection(state, state_prev, dt_i)
+            self.system.project_onto_tangent_space(state)
+            state_back = state.copy()
+            self.step_b_inner(state_back, -dt_i)
+            self.system.project_onto_manifold(state_back, state)
+            rev_diff = self.reverse_check_norm(state_back.pos - state_prev.pos)
+            if rev_diff > self.reverse_check_tol:
                 raise IntegratorError(
-                    f'Non-reversible step. Difference between initial and '
-                    f'forward-backward positions = {rev_diff:.1e}.')
+                    f'Non-reversible step. Distance between initial and '
+                    f'forward-backward integrated positions = {rev_diff:.1e}.')
 
     def step(self, state):
         dt = state.dir * self.step_size
         state = state.copy()
         self.step_a(state, 0.5 * dt)
+        self.system.project_onto_tangent_space(state)
         self.step_b(state, dt)
         self.step_a(state, 0.5 * dt)
+        self.system.project_onto_tangent_space(state)
         return state
+
+
+class GeodesicConstrainedLeapfrogIntegrator(BaseConstrainedLeapfrogIntegrator):
+
+    def step_a(self, state, dt):
+        state.mom -= dt * self.system.dh_dpos(state)
+
+    def step_b_inner(self, state, dt):
+        state.pos += dt * self.system.dh_dmom(state)
+
+    def solve_for_mom_post_projection(self, state, state_prev, dt):
+        state.mom = self.system.solve_dh_dmom_for_mom(
+            (state.pos - state_prev.pos) / dt)
+
+
+class SplitConstrainedLeapfrogIntegrator(BaseConstrainedLeapfrogIntegrator):
+
+    def step_a(self, state, dt):
+        state.mom -= dt * self.system.dh1_dpos(state)
+
+    def step_b_inner(self, state, dt):
+        self.system.h2_exact_flow(state, dt)
+
+    def solve_for_mom_post_projection(self, state, state_prev, dt):
+        state.mom = self.system.solve_h2_flow_for_mom_gvn_pos(
+            state.pos, state_prev.pos, dt)
