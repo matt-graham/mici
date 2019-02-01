7ce32e9df703135e9b7a36492f3ca13009fb33bb
Matt Graham <matthew.m.graham@gmail.com>
2019-02-01 17:47:55 +0800

    Update __init__.py with new module imports and remove __all__

--- hmc/__init__.py
+++ hmc/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """ Python implementations of Hamiltonian dynamics based MCMC samplers. """
 
-__all__ = ['integrators', 'samplers', 'solvers', 'states', 'systems', 'utils']
 __authors__ = 'Matt Graham'
 __license__ = 'MIT'
 
 import hmc.integrators
+import hmc.metrics
 import hmc.samplers
 import hmc.solvers
 import hmc.states
 import hmc.systems
+import hmc.transitions
 import hmc.utils
