7d172a672de43e26703deb8290c8c36a41554324
Matt Graham <matthew.m.graham@gmail.com>
2019-03-05 15:59:06 +0800

    Fixing autograd import checking
    Puts all functions using objects from autograd namespace within an 
    conditional block that is only run if autograd is available.

--- hmc/autograd_wrapper.py
+++ hmc/autograd_wrapper.py
@@ -11,106 +11,106 @@ try:
 except ImportError:
     AUTOGRAD_AVAILABLE = False
 
+if AUTOGRAD_AVAILABLE:
 
-@unary_to_nary
-def grad_and_value(fun, x):
-    """
-    Returns a function that returns both gradient and value of a function.
-    """
-    vjp, val = make_vjp(fun, x)
-    if not vspace(val).size == 1:
-        raise TypeError("grad_and_value only applies to real scalar-output "
-                        "functions.")
-    return vjp(vspace(val).ones()), val
-
-
-@unary_to_nary
-def jacobian_and_value(fun, x):
-    """
-    Returns a function that returns both the Jacobian and value of a function.
-
-    Assumes that the function `fun` broadcasts along the first dimension of the
-    input being differentiated with respect to such that a batch of outputs can
-    be computed concurrently for a batch of inputs.
-    """
-    val = fun(x)
-    v_vspace = vspace(val)
-    x_vspace = vspace(x)
-    x_rep = np.tile(x, (v_vspace.size,) + (1,) * x_vspace.ndim)
-    vjp_rep, _ = make_vjp(fun, x_rep)
-    jacobian_shape = v_vspace.shape + x_vspace.shape
-    basis_vectors = np.array([b for b in v_vspace.standard_basis()])
-    jacobian = vjp_rep(basis_vectors)
-    return np.reshape(jacobian, jacobian_shape), val
-
-
-@unary_to_nary
-def mhp_jacobian_and_value(fun, x):
-    """
-    Returns a function that returns MHP, Jacobian and value of a function.
-
-    For a vector-valued function `fun` the matrix-Hessian-product (MHP) is here
-    defined as a function of a matrix `m` corresponding to
-
-        mhp(m) = sum(m[:, :] * h[:, :, :], axis=(-1, -2))
-
-    where `h` is the vector-Hessian of `f = fun(x)` wrt `x` i.e. the rank-3
-    tensor of second-order partial derivatives of the vector-valued function,
-    such that
-
-        h[k, i, j] = (d**2 f[i]) / (dx[j] * dx[k])
-
-    Assumes that the function `fun` broadcasts along the first dimension of the
-    input being differentiated with respect to such that a batch of outputs can
-    be computed concurrently for a batch of inputs.
-    """
-    mhp, (jacob, val) = make_vjp(
-        lambda x: atuple(jacobian_and_value(fun)(x)), x)
-    return lambda m: mhp((m, vspace(val).zeros())), jacob, val
-
-
-@unary_to_nary
-def hessian_grad_and_value(fun, x):
-    """
-    Returns a function that returns Hessian, gradient and value of a function.
-
-    Assumes that the function `fun` broadcasts along the first dimension of the
-    input being differentiated with respect to such that a batch of outputs can
-    be computed concurrently for a batch of inputs.
-    """
-    def grad_fun(x):
+    @unary_to_nary
+    def grad_and_value(fun, x):
+        """
+        Returns a function that returns both gradient and value of a function.
+        """
         vjp, val = make_vjp(fun, x)
+        if not vspace(val).size == 1:
+            raise TypeError("grad_and_value only applies to real scalar-output"
+                            " functions.")
         return vjp(vspace(val).ones()), val
-    x_vspace = vspace(x)
-    x_rep = np.tile(x, (x_vspace.size,) + (1,) * x_vspace.ndim)
-    vjp_grad, (grad, val) = make_vjp(lambda x: atuple(grad_fun(x)), x_rep)
-    hessian_shape = x_vspace.shape + x_vspace.shape
-    basis_vectors = np.array([b for b in x_vspace.standard_basis()])
-    hessian = vjp_grad((basis_vectors, vspace(val).zeros()))
-    return np.reshape(hessian, hessian_shape), grad[0], val[0]
 
+    @unary_to_nary
+    def jacobian_and_value(fun, x):
+        """
+        Returns a function that returns both the Jacobian and value of a
+        function.
 
-@unary_to_nary
-def mtp_hessian_grad_and_value(fun, x):
-    """
-    Returns a function that returns MTP, Jacobian and value of a function.
+        Assumes that the function `fun` broadcasts along the first dimension of
+        the input being differentiated with respect to such that a batch of
+        outputs can be computed concurrently for a batch of inputs.
+        """
+        val = fun(x)
+        v_vspace = vspace(val)
+        x_vspace = vspace(x)
+        x_rep = np.tile(x, (v_vspace.size,) + (1,) * x_vspace.ndim)
+        vjp_rep, _ = make_vjp(fun, x_rep)
+        jacobian_shape = v_vspace.shape + x_vspace.shape
+        basis_vectors = np.array([b for b in v_vspace.standard_basis()])
+        jacobian = vjp_rep(basis_vectors)
+        return np.reshape(jacobian, jacobian_shape), val
 
-    For a scalar-valued function `fun` the matrix-Tressian-product (MTP) is
-    here defined as a function of a matrix `m` corresponding to
+    @unary_to_nary
+    def mhp_jacobian_and_value(fun, x):
+        """
+        Returns a function that returns MHP, Jacobian and value of a function.
 
-        mtp(m) = sum(m[:, :] * t[:, :, :], axis=(-1, -2))
+        For a vector-valued function `fun` the matrix-Hessian-product (MHP) is
+        here defined as a function of a matrix `m` corresponding to
 
-    where `t` is the 'Tressian' of `f = fun(x)` wrt `x` i.e. the rank-3 tensor
-    of third-order partial derivatives of the scalar-valued function, such that
+            mhp(m) = sum(m[:, :] * h[:, :, :], axis=(-1, -2))
 
-        t[i, j, k] = d**3 f / (dx[i] * dx[j] * dx[k])
+        where `h` is the vector-Hessian of `f = fun(x)` wrt `x` i.e. the rank-3
+        tensor of second-order partial derivatives of the vector-valued
+        function, such that
 
-    Assumes that the function `fun` broadcasts along the first dimension of the
-    input being differentiated with respect to such that a batch of outputs can
-    be computed concurrently for a batch of inputs.
-    """
-    mtp, (hessian, grad, val) = make_vjp(
-        lambda x: atuple(hessian_grad_and_value(fun)(x)), x)
-    return (
-        lambda m: mtp((m, vspace(grad).zeros(), vspace(val).zeros())),
-        hessian, grad, val)
+            h[k, i, j] = (d**2 f[i]) / (dx[j] * dx[k])
+
+        Assumes that the function `fun` broadcasts along the first dimension of
+        the input being differentiated with respect to such that a batch of
+        outputs can be computed concurrently for a batch of inputs.
+        """
+        mhp, (jacob, val) = make_vjp(
+            lambda x: atuple(jacobian_and_value(fun)(x)), x)
+        return lambda m: mhp((m, vspace(val).zeros())), jacob, val
+
+    @unary_to_nary
+    def hessian_grad_and_value(fun, x):
+        """
+        Returns a function that returns the Hessian, gradient and value of a
+        function.
+
+        Assumes that the function `fun` broadcasts along the first dimension of
+        the input being differentiated with respect to such that a batch of
+        outputs can be computed concurrently for a batch of inputs.
+        """
+        def grad_fun(x):
+            vjp, val = make_vjp(fun, x)
+            return vjp(vspace(val).ones()), val
+        x_vspace = vspace(x)
+        x_rep = np.tile(x, (x_vspace.size,) + (1,) * x_vspace.ndim)
+        vjp_grad, (grad, val) = make_vjp(lambda x: atuple(grad_fun(x)), x_rep)
+        hessian_shape = x_vspace.shape + x_vspace.shape
+        basis_vectors = np.array([b for b in x_vspace.standard_basis()])
+        hessian = vjp_grad((basis_vectors, vspace(val).zeros()))
+        return np.reshape(hessian, hessian_shape), grad[0], val[0]
+
+    @unary_to_nary
+    def mtp_hessian_grad_and_value(fun, x):
+        """
+        Returns a function that returns MTP, Jacobian and value of a function.
+
+        For a scalar-valued function `fun` the matrix-Tressian-product (MTP) is
+        here defined as a function of a matrix `m` corresponding to
+
+            mtp(m) = sum(m[:, :] * t[:, :, :], axis=(-1, -2))
+
+        where `t` is the 'Tressian' of `f = fun(x)` wrt `x` i.e. the rank-3
+        tensor of third-order partial derivatives of the scalar-valued function
+        such that
+
+            t[i, j, k] = d**3 f / (dx[i] * dx[j] * dx[k])
+
+        Assumes that the function `fun` broadcasts along the first dimension of
+        the input being differentiated with respect to such that a batch of
+        outputs can be computed concurrently for a batch of inputs.
+        """
+        mtp, (hessian, grad, val) = make_vjp(
+            lambda x: atuple(hessian_grad_and_value(fun)(x)), x)
+        return (
+            lambda m: mtp((m, vspace(grad).zeros(), vspace(val).zeros())),
+            hessian, grad, val)
