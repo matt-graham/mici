dd0f7ca0f8fab1ff6eb7a1cbed9c73e59dada658
Matt Graham <matthew.m.graham@gmail.com>
2019-10-23 18:35:29 +0800

    Updating setup script for publishing to PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
-#!/usr/bin/env python2
+import setuptools
 
-from setuptools import setup
-
-if __name__ == '__main__':
-    setup(name='mici',
-          description=(
-              'MCMC samplers based on simulating Hamiltonian dynamics on a '
-              'manifold'),
-          author='Matt Graham',
-          url='https://github.com/matt-graham/hamiltonian-monte-carlo.git',
-          packages=['mici'])
+setuptools.setup(
+    name='mici',
+    version='0.1.0',
+    author='Matt Graham',
+    description=(
+        'MCMC samplers based on simulating Hamiltonian dynamics on a manifold'
+    ),
+    long_description=(
+        'Mici is a Python package providing implementations of Markov chain '
+        'Monte Carlo (MCMC) methods for approximate inference in probabilistic'
+        ' models, with a particular focus on MCMC methods based on simulating '
+        'Hamiltonian dynamics on a manifold.'
+    ),
+    url='https://github.com/matt-graham/mici.git',
+    packages=['mici'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers'
+    ],
+    keywords='inference sampling MCMC HMC',
+    license='MIT',
+    license_file='LICENSE',
+    install_requires=['numpy>=1.15', 'scipy>=1.1'],
+    python_requires='>=3.6'
+)
