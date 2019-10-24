3a3df22f3a9d151eea087036f170a0b5fbbe381f
Matt Graham <matthew.m.graham@gmail.com>
2019-10-24 22:20:58 +0800

    Removing no longer used inherit_docstrings decorator

--- mici/utils.py
+++ mici/utils.py
@@ -172,36 +172,11 @@ def get_size(obj, seen=None):
     return size
 
 
-def inherit_docstrings(cls):
-    """Class decorator to inherit parent class docstrings if not specified.
-
-    Taken from https://stackoverflow.com/a/38601305/4798943.
-    """
-    for name, func in vars(cls).items():
-        if isinstance(func, types.FunctionType) and not func.__doc__:
-            for parent in cls.__bases__:
-                parfunc = getattr(parent, name, None)
-                if parfunc and getattr(parfunc, '__doc__', None):
-                    func.__doc__ = parfunc.__doc__
-                    break
-        elif isinstance(func, property) and not func.fget.__doc__:
-            for parent in cls.__bases__:
-                parprop = getattr(parent, name, None)
-                if parprop and getattr(parprop.fget, '__doc__', None):
-                    newprop = property(fget=func.fget,
-                                       fset=func.fset,
-                                       fdel=func.fdel,
-                                       doc=parprop.fget.__doc__)
-                    setattr(cls, name, newprop)
-                    break
-
-    return cls
-
-
 LOG_2 = log(2.)
 
 
 def log1p_exp(val):
+    """Numerically stable implementation of `log(1 + exp(val))`."""
     if val > 0.:
         return val + log1p(exp(-val))
     else:
