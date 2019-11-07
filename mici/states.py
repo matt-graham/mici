acc75685abacde5dbf3d14419c588cb62ebba30f
Matt Graham <matthew.m.graham@gmail.com>
2019-11-07 19:56:20 +0800

    Adding facility to count system function calls in state object
    Also allowing pickling of non-function state cached values which saves a 
    few cache misses after transferring initial chain states to new 
    processes when sampling parallel chains.

--- mici/states.py
+++ mici/states.py
@@ -19,18 +19,23 @@ def cache_in_state(*depends_on):
     def cache_in_state_decorator(method):
         @wraps(method)
         def wrapper(self, state):
-            key = (type(self).__name__ + '.' + method.__name__, id(self))
+            key = type(self).__name__ + '.' + method.__name__
             if key not in state._cache:
                 for dep in depends_on:
                     state._dependencies[dep].add(key)
             if key not in state._cache or state._cache[key] is None:
                 state._cache[key] = method(self, state)
+                if state._call_counts is not None:
+                    if key not in state._call_counts:
+                        state._call_counts[key] = 1
+                    else:
+                        state._call_counts[key] += 1
             return state._cache[key]
         return wrapper
     return cache_in_state_decorator
 
 
-def multi_cache_in_state(depends_on, vars, primary_index=0):
+def multi_cache_in_state(depends_on, variables, primary_index=0):
     """Decorator to cache multiple outputs of a function of state variable(s).
 
     Used to wrap functions of a chain state vaiable(s) to allow caching of
@@ -46,19 +51,18 @@ def multi_cache_in_state(depends_on, vars, primary_index=0):
             variables the computed values depend on e.g. `['pos', 'mom']`, such
             that the cache is correctly cleared when one of these parent
             dependency's value changes.
-        vars (List[str]): A list of strings defining the variables in the state
-            cache dict corresponding to the outputs of the wrapped function
-            (method) in the corresponding returned order.
+        variables (List[str]): A list of strings defining the variables in the
+            state cache dict corresponding to the outputs of the wrapped
+            function (method) in the corresponding returned order.
         primary_index (int): Index of primary output of function (i.e. value to
-            be returned) in vars list / position in output of function.
+            be returned) in `variables` list / position in output of function.
     """
     def multi_cache_in_state_decorator(method):
         @wraps(method)
         def wrapper(self, state):
-            id_ = id(self)
             type_prefix = type(self).__name__ + '.'
-            prim_key = (type_prefix + vars[primary_index], id_)
-            keys = [(type_prefix + v, id_) for v in vars]
+            prim_key = type_prefix + variables[primary_index]
+            keys = [type_prefix + v for v in variables]
             for i, key in enumerate(keys):
                 if key not in state._cache:
                     for dep in depends_on:
@@ -70,6 +74,11 @@ def multi_cache_in_state(depends_on, vars, primary_index=0):
                         state._cache[k] = v
                 else:
                     state._cache[prim_key] = vals
+                if state._call_counts is not None:
+                    if prim_key not in state._call_counts:
+                        state._call_counts[prim_key] = 1
+                    else:
+                        state._call_counts[prim_key] += 1
             return state._cache[prim_key]
         return wrapper
     return multi_cache_in_state_decorator
@@ -83,7 +92,8 @@ class ChainState(object):
     values are subsequently reused.
     """
 
-    def __init__(self, _dependencies=None, _cache=None, **vars):
+    def __init__(self, _dependencies=None, _cache=None, _call_counts=None,
+                 **variables):
         """Create a new `ChainState` instance.
 
         Any keyword arguments passed to the constructor will be used to set
@@ -94,30 +104,31 @@ class ChainState(object):
         will return a `ChainState` instance `state` with variable attributes
         `state.pos`, `state.mom` and `state.dir` with initial values set to
         `pos_val`, `mom_val` and `dir_val` respectively. The keyword arguments
-        `_dependencies` and `_cache` are reserved for the dependency set and
-        cache dictionary respectively used to implement the caching of derived
-        quantities and cannot be used as state variable names.
+        `_dependencies`, `_cache` and `_call_counts` are reserved respectively
+        for the dependency set, cache dictionary and call count dictionary, and
+        cannot be used as state variable names.
         """
-        # set vars attribute by directly writing to __dict__ to ensure set
-        # before any cally to __setattr__
-        self.__dict__['vars'] = vars
+        # Set _variables attribute by directly writing to __dict__ to ensure
+        # set before any cally to __setattr__
+        self.__dict__['_variables'] = variables
         if _dependencies is None:
-            _dependencies = {name: set() for name in vars}
+            _dependencies = {name: set() for name in variables}
         self._dependencies = _dependencies
         if _cache is None:
             _cache = {}
         self._cache = _cache
+        self._call_counts = _call_counts
 
     def __getattr__(self, name):
-        if name in self.vars:
-            return self.vars[name]
+        if name in self._variables:
+            return self._variables[name]
         else:
             raise AttributeError(
                 f"'{type(self).__name__}' object has no attribute '{name}'")
 
     def __setattr__(self, name, value):
-        if name in self.vars:
-            self.vars[name] = value
+        if name in self._variables:
+            self._variables[name] = value
             # clear any dependent cached values
             for dep in self._dependencies[name]:
                 self._cache[dep] = None
@@ -125,23 +136,24 @@ class ChainState(object):
             return super().__setattr__(name, value)
 
     def __contains__(self, name):
-        return name in self.vars
+        return name in self._variables
 
     def copy(self):
         """Create a deep copy of the state object.
 
         Returns:
             state_copy (ChainState): A copy of the state object which can be
-                updated without affecting the original object's attributes.
+                updated without affecting the original object's variables.
         """
         return type(self)(
-            _cache=self._cache.copy(), _dependencies=self._dependencies,
-            **{name: copy.copy(val) for name, val in self.vars.items()})
+            _dependencies=self._dependencies, _cache=self._cache.copy(),
+            _call_counts=self._call_counts,
+            **{name: copy.copy(val) for name, val in self._variables.items()})
 
     def __str__(self):
         return (
             '(\n ' +
-            ',\n '.join([f'{name}={val}' for name, val in self.vars.items()]) +
+            ',\n '.join([f'{k}={v}' for k, v in self._variables.items()]) +
             ')'
         )
 
@@ -149,9 +161,16 @@ class ChainState(object):
         return type(self).__name__ + str(self)
 
     def __getstate__(self):
-        return self.vars
+        return {
+            'variables': self._variables,
+            'dependencies': self._dependencies,
+            # Don't pickle callable cached 'variables' such as derivative
+            # functions
+            'cache': {k: v for k, v in self._cache.items() if not callable(v)},
+            'call_counts': self._call_counts}
 
     def __setstate__(self, state):
-        self.__dict__['vars'] = state
-        self._dependencies = {name: set() for name in self.vars}
-        self._cache = {}
+        self.__dict__['_variables'] = state['variables']
+        self._dependencies = state['dependencies']
+        self._cache = state['cache']
+        self._call_counts = state['call_counts']
