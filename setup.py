e040e0d6af2dee888400efa5e62681bd24c32dc3
Matt Graham <matthew.m.graham@gmail.com>
2019-12-11 19:22:47 +0800

    Updating version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mici',
-    version='0.1.2',
+    version='0.1.3',
     author='Matt Graham',
     description=(
         'MCMC samplers based on simulating Hamiltonian dynamics on a manifold'
