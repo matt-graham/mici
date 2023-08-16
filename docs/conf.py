1c08190ed0d1b24a841cacda6d43c62d1e7534a4
Matt Graham <matthew.m.graham@gmail.com>
2023-08-16 10:16:34 +0100

    Change Sphinx documentation to pydata-sphinx-theme

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,23 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static", "../images"]
+
+html_theme_options = {
+    "logo": {
+        "image_light": "../images/mici-logo-rectangular.svg",
+        "image_dark": "../images/mici-logo-rectangular-light-text.svg",
+    },
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/matt-graham/mici",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+        },
+    ],
+}
 
 
 # -- Post process ------------------------------------------------------------
