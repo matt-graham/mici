1749cba78a4ba69bd51b1e5007d06f78f0fd8cb7
Matt Graham <matthew.m.graham@gmail.com>
2024-10-05 20:27:26 +0100

    Make autodiff subpackage + add details of backends to docstring

--- src/mici/autodiff/__init__.py
+++ src/mici/autodiff/__init__.py
@@ -1,10 +1,39 @@
-"""Automatic differentation fallback for constructing derivative functions."""
+"""Automatic differentation support for constructing derivative functions.
+
+Multiple automatic differentiation backends are supported:
+
+* `jax`: High performance array computing framework, with support for running
+  computations on accelerator devices and just-in-time (JIT) compilation. To
+  differentiate a function using the `jax` backend, the function must be defined in
+  terms of JAX primitives, for example using the functions in the `jax.numpy` API. By
+  default the derivative functions produced will be JIT compiled; a `jax_nojit` variant
+  is available if the function (or its derivative) is not compatible with JIT
+  compilation.
+* `autograd`: Autograd can automatically differentiate native Python and NumPy code. To
+  differentiate a function using the `autograd` backend it should be defined in terms
+  of functions from the `autograd.numpy` and `autograd.scipy` APIs. Compared to JAX,
+  the lack of JIT compilation in Autograd and features such as automatic vectorisation
+  make Autograd slower, and so JAX will generally be a better choice.
+* `symnum`: SymNum is a Python package that acts a bridge between NumPy and SymPy,
+  providing a NumPy-like interface that can be used to symbolically define functions
+  which take arrays as arguments and return arrays or scalars as values. To
+  differentiate a function using the `symnum` backend it should be defined in terms of
+  functions from the `symnum.numpy` API, and should have been decorated with
+  ``symnum.numpify`` with specified argument shapes. SymNum is intended for use in
+  generating the derivatives of 'simple' functions which compose a relatively small
+  number of operations and act on small array inputs. By reducing interpreter overheads
+  it can produce code which is cheaper to evaluate than corresponding Autograd or JAX
+  functions (including those using JIT compilation) in such cases, and which can be
+  serialised with the inbuilt Python pickle library allowing use for example in
+  libraries which use multiprocessing to implement parallelisation across multiple
+  processes.
+"""
 
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, NamedTuple
 
-from mici import autograd_wrapper, jax_wrapper, symnum_wrapper
+from mici.autodiff import autograd_wrapper, jax_wrapper, symnum_wrapper
 
 if TYPE_CHECKING:
     from types import ModuleType
@@ -68,7 +97,8 @@ Defaults to first available backend from `jax`, `autograd` and `symnum` (in that
 or to `None` if none are available.
 """
 DEFAULT_BACKEND = next(
-    (name for name, backend in _REGISTERED_BACKENDS.items() if backend.available), None,
+    (name for name, backend in _REGISTERED_BACKENDS.items() if backend.available),
+    None,
 )
 
 
