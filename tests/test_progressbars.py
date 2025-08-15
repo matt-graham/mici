9de99bb01c206a929f679bd33ebfa8412f606a0b
Matt Graham <matthew.m.graham@gmail.com>
2025-08-15 14:28:30 +0100

    Add test for in interactive shell function

--- tests/test_progressbars.py
+++ tests/test_progressbars.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from queue import SimpleQueue
+from unittest import mock
 
 import pytest
 
@@ -11,13 +12,29 @@ def test_format_time():
     assert mici.progressbars._format_time(10000) == "2:46:40"
 
 
-def test_stats_runnin_means():
+def test_stats_running_means():
     means = {}
     vals = {"a": 1, "b": 2}
     mici.progressbars._update_stats_running_means(1, means, vals)
     assert means == vals
     mici.progressbars._update_stats_running_means(2, means, vals)
     assert means == vals
+
+
+@pytest.mark.parametrize("ipython_available", [True, False])
+@pytest.mark.parametrize("in_ipython_shell", [True, False])
+def test_in_interactive_shell(monkeypatch, ipython_available, in_ipython_shell):
+    monkeypatch.setattr("mici.progressbars.IPYTHON_AVAILABLE", ipython_available)
+    if in_ipython_shell:
+        ipython_mock = mock.Mock()
+        ipython_mock.__module__ = "ipykernel.zmqshell"
+        ipython_mock.__class__.__name__ = "ZMQInteractiveShell"
+    else:
+        ipython_mock = None
+    monkeypatch.setattr("mici.progressbars.get_ipython", lambda: ipython_mock)
+    assert mici.progressbars._in_interactive_shell() == (
+        ipython_available and in_ipython_shell
+    )
 
 
 @pytest.fixture(
